83855c2ee33cd258ec4806ca157aeb276e916cf2
TARUN MISTRY <mistrytarun2000@gmail.com>
2024-06-27 00:33:39 +0530

    commit

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -2,8 +2,10 @@ import React from "react";
 
 function About() {
   return (
-    <div>
-      About
+    <div className="w-full h-screen py-[20vh]">
+      <h1 className="text-8xl text-center font-['Founders_Grotesk'] ">Tarun</h1>
+
+      <h1 className="relative text-8xl text-center font-['Founders_Grotesk'] ">Mistry</h1>
     </div>
   )
 }
