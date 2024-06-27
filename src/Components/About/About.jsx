e8c2324e3b4fb2900030d1ee4fe718cf463051d9
TARUN MISTRY <mistrytarun2000@gmail.com>
2024-06-27 23:54:57 +0530

    need to complete about section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -4,8 +4,11 @@ function About() {
   return (
     <div className="w-full h-screen py-[20vh]">
       <h1 className="text-8xl text-center font-['Founders_Grotesk'] ">Tarun</h1>
-
       <h1 className="relative text-8xl text-center font-['Founders_Grotesk'] ">Mistry</h1>
+      <div className="about-video-wrapper">
+        <video className="about-video" src="https://www.youtube.com/watch?v=6ZfZGjKtXnE" autoPlay loop muted></video>
+
+      </div>
     </div>
   )
 }
